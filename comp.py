c7c5cca496dba884336184f5f208a2e37b6f0a5f
William Henderson <tgpz35@durham.ac.uk>
2023-07-13 13:57:11 +0100

    updated my solution

--- comp.py
+++ comp.py
@@ -1,16 +1,27 @@
-def comp(a, b):
+#my solution
+"""def comp(a, b):
 
-    new_list = [x for x in a if x**2 in b]
-    
-    if len(new_list) == len(a):
-        return True
-
-    else:
+    if a is None or b is None or len(a) != len(b):
         return False
+        
+    count_a = {}
+    count_b = {}
 
-a = [121, 144, 19, 161, 19, 144, 19, 11]  
-b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+    for num in a:
+        count_a[num] = count_a.get(num, 0) + 1
+
+    for num in b:
+        count_b[num] = count_b.get(num, 0) + 1
+
+    for num in count_a:
+        if num ** 2 not in count_b or count_a[num] != count_b[num ** 2]:
+            return False
+
+    return True
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 
 same = comp(a,b)
 
-print(same)
+print(same)"""
