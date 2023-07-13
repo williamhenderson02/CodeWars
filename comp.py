b28cd38c8f30f999845e9a27388360abf2454605
William Henderson <tgpz35@durham.ac.uk>
2023-07-13 14:02:07 +0100

    best solution for comp

--- comp.py
+++ comp.py
@@ -25,3 +25,16 @@ b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
 same = comp(a,b)
 
 print(same)"""
+
+def comp(a, b):
+    try:
+        return sorted([i ** 2 for i in a]) == sorted(b)
+    except:
+        return False
+    
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+same = comp(a,b)
+
+print(same)
