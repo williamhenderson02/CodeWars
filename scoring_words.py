b2e946a448b100d03c6b10d97a59bf3e3876f360
William Henderson <tgpz35@durham.ac.uk>
2023-07-14 15:00:18 +0100

    my solution for scoring words

---
+++ scoring_words.py
@@ -0,0 +1,25 @@
+#my solution
+def high(x):
+
+    highest = ''
+
+    text = x.split()
+
+    maxi = 0
+
+    for word in text:
+        total = 0
+        for char in word:
+            num = ord(char) - 96
+            total = total + num
+            if total > maxi:
+                highest = word
+                maxi = total
+
+    return highest
+
+x = 'what time are we climbing up the volcano'
+
+highest = high(x)
+
+print(highest)
