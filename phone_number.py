59d02f5d5ee8664888133110ffdaac4124dccb02
William Henderson <tgpz35@durham.ac.uk>
2023-07-15 14:30:48 +0100

    added '.format' solution

--- phone_number.py
+++ phone_number.py
@@ -1,7 +1,18 @@
 #my one liner solution
-def create_phone_number(n):
+"""def create_phone_number(n):
 
     return "(" + ''.join(str(x) for x in n[0:3]) + ") " + ''.join(str(y) for y in n[3:6]) + '-' + ''.join(str(z) for z in n[6:10])
+
+n = [1,2,3,4,5,6,7,8,9,0]
+
+number = create_phone_number(n)
+
+print(number)"""
+
+#more elegant one liner using .format
+def create_phone_number(n):
+	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
 
 n = [1,2,3,4,5,6,7,8,9,0]
 
